176bd67f515a0f0b8cd638e8928696fd6e7e2e05
Kashish Shukla <kashish.shukla.8076@gmail.com>
2026-09-16 16:31:09 +0530

    chatbot using threads

---
+++ langgraph_chatbot/backend.py
@@ -0,0 +1,36 @@
+from langgraph.graph import StateGraph, START, END
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from typing import TypedDict,Annotated
+import os
+from dotenv import load_dotenv
+from langgraph.graph.message import add_messages
+from langchain_core.messages import BaseMessage
+from langchain_core.messages import SystemMessage, HumanMessage
+from langgraph.checkpoint.memory import MemorySaver
+
+load_dotenv()
+
+endpoint=HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
+    task="text-generation"
+    )
+model=ChatHuggingFace(llm=endpoint)
+
+class ChatState(TypedDict):
+    messages:Annotated[list[BaseMessage],add_messages]
+
+def chatNode(state:ChatState):
+    messages=state['messages']
+    response=model.invoke(messages)
+    return {"messages":response}
+
+checkpointer=MemorySaver()
+
+graph=StateGraph(ChatState)
+
+graph.add_node("chatNode",chatNode)
+graph.add_edge(START,"chatNode")
+graph.add_edge("chatNode",END)
+
+chatbot=graph.compile(checkpointer=checkpointer)
